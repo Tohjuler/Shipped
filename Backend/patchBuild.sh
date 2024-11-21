b49eba7e83a3e020b4f6fc1b7ac73cc2c824f9d8
Tohjuler <tobias@tohjuler.dk>
2024-11-21 12:03:09 +0100

    fix: Docker image for backend

---
+++ Backend/patchBuild.sh
@@ -0,0 +1,6 @@
+mkdir -p node_modules/@electric-sql/pglite && touch node_modules/@electric-sql/pglite/index.js
+mkdir -p node_modules/@vercel/postgres && touch node_modules/@vercel/postgres/index.js
+mkdir -p node_modules/@planetscale/database && touch node_modules/@planetscale/database/index.js
+mkdir -p node_modules/@neondatabase/serverless && touch node_modules/@neondatabase/serverless/index.js
+mkdir -p node_modules/@aws-sdk/client-rds-data && touch node_modules/@aws-sdk/client-rds-data/index.js
+mkdir -p node_modules/pg && touch node_modules/pg/index.js
